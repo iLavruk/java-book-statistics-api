cbca950ae279b652a40d85f8cf52ddd30641468b
Ivan Lavruk <ilavr8725@gmail.com>
2025-12-07 18:15:30 +0100

    Remove unused book repository method

--- src/main/java/com/profitsoft/bookstats/repository/BookRepository.java
+++ src/main/java/com/profitsoft/bookstats/repository/BookRepository.java
@@ -2,16 +2,12 @@ package com.profitsoft.bookstats.repository;
 
 import com.profitsoft.bookstats.domain.Book;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-
-  @EntityGraph(attributePaths = "author")
-  Optional<Book> findWithAuthorById(Long id);
 
   @Query("select b from Book b join fetch b.author where b.id = :id")
   Optional<Book> fetchByIdWithAuthor(@Param("id") Long id);
